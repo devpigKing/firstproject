2d11beb0ed1bbabbf07f7c56b78ee91ac403cac1
devpigKing <devpigking@gmail.com>
2024-01-01 12:37:09 +0900

    refactor : lombok을 통한 Entity 리팩토링

--- src/main/java/com/example/firstproject/entity/Article.java
+++ src/main/java/com/example/firstproject/entity/Article.java
@@ -1,8 +1,14 @@
 package com.example.firstproject.entity;
 
 import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.NoArgsConstructor;
+import lombok.ToString;
 
 @Entity // 1. 엔티티임을 선언, 이 어노테이션이 붙은 클래스를 기반으로 DB에 테이블이 생성됨.
+@AllArgsConstructor
+@ToString
+@NoArgsConstructor
 public class Article {
     @Id // 3. 엔티티의 대표값 지정
     @GeneratedValue(strategy = GenerationType.IDENTITY) // 자동 생성. 기본키 생성은 DB가
@@ -14,22 +20,22 @@ public class Article {
     @Column // 2. content 필드 선언
     private String content;
 
-    public Article(Long id, String title, String content) {
-        this.id = id;
-        this.title = title;
-        this.content = content;
-    }
+//    public Article(Long id, String title, String content) {
+//        this.id = id;
+//        this.title = title;
+//        this.content = content;
+//    }
 
-    public Article() {
+//    public Article() {
+//
+//    }
 
-    }
-
-    @Override
-    public String toString() {
-        return "Article{" +
-                "id=" + id +
-                ", title='" + title + '\'' +
-                ", content='" + content + '\'' +
-                '}';
-    }
+//    @Override
+//    public String toString() {
+//        return "Article{" +
+//                "id=" + id +
+//                ", title='" + title + '\'' +
+//                ", content='" + content + '\'' +
+//                '}';
+//    }
 }
