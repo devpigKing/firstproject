2eb5ac6306e4763455a84d7dc6de606fcb10054b
devpigKing <devpigking@gmail.com>
2024-01-01 12:37:25 +0900

    refactor : lombok을 통한 컨트롤러의 println()문 리팩토링

--- src/main/java/com/example/firstproject/controller/ArticleController.java
+++ src/main/java/com/example/firstproject/controller/ArticleController.java
@@ -3,11 +3,13 @@ package com.example.firstproject.controller;
 import com.example.firstproject.dto.ArticleForm;
 import com.example.firstproject.entity.Article;
 import com.example.firstproject.repository.ArticleRepository;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 
+@Slf4j // 로깅 기능을 위한 어노테이션 추가
 @Controller
 public class ArticleController {
 
@@ -22,13 +24,16 @@ public class ArticleController {
 
     @PostMapping("/articles/create") // URL 요청 접수
     public String createArticle(ArticleForm form) { // 메서드 생성 및 반환값 작성
-        System.out.println(form.toString());
+        //System.out.println(form.toString());
+        log.info(form.toString());
         // 1. DTO를 엔티티로 변환
         Article article = form.toEntity();
-        System.out.println(article.toString());
+        //System.out.println(article.toString());
+        log.info(article.toString());
         // 2. 레포지토리로 엔티티를 DB에 저장
         Article saved = articleRepository.save(article); // article 엔티티를 저장해 saved 객체에 반환
-        System.out.println(article.toString());
+        //System.out.println(article.toString());
+        log.info(article.toString());
 
         return "";
     }
