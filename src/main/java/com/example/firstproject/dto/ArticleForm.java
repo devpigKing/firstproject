172a4c15f890de306b13895e25fe31edad634bb8
devpigKing <devpigking@gmail.com>
2024-01-01 12:37:02 +0900

    refactor : lombok을 통한 DTO 리팩토링

--- src/main/java/com/example/firstproject/dto/ArticleForm.java
+++ src/main/java/com/example/firstproject/dto/ArticleForm.java
@@ -1,24 +1,28 @@
 package com.example.firstproject.dto;
 
 import com.example.firstproject.entity.Article;
+import lombok.AllArgsConstructor;
+import lombok.ToString;
 
+@AllArgsConstructor // 모든 필드를 매개변수로 하는 생성자 자동 생성
+@ToString // toString() 메서드 생성
 public class ArticleForm {
     private String title; // 제목을 받을 필드
     private String content; // 내용을 받을 필드
 
-    public ArticleForm(String title, String content) {
-        this.title = title;
-        this.content = content;
-    }
+//    public ArticleForm(String title, String content) {
+//        this.title = title;
+//        this.content = content;
+//    }
 
-    // 데이터를 잘 받았는지 체크할 toString()
-    @Override
-    public String toString() {
-        return "ArticleForm{" +
-                "title='" + title + '\'' +
-                ", content='" + content + '\'' +
-                '}';
-    }
+//    // 데이터를 잘 받았는지 체크할 toString()
+//    @Override
+//    public String toString() {
+//        return "ArticleForm{" +
+//                "title='" + title + '\'' +
+//                ", content='" + content + '\'' +
+//                '}';
+//    }
 
     public Article toEntity() {
         return new Article(null, title, content);
